ced3f31996904701479f7f0a8331125dfdb7fdc3
git-sergBond <git-serg.bond@yandex.ru>
2017-06-26 18:40:09 +0400

    add class and xml for Product

---
+++ app/src/main/java/com/example/home/testarradaptersapp/Product.java
@@ -0,0 +1,37 @@
+package com.example.home.testarradaptersapp;
+
+/**
+ * Created by Home on 26.06.2017.
+ */
+
+public class Product {
+    private String name, unit;
+    private int count;
+
+    Product(String name, int count, String unit){
+        this.name = name;
+        this.count = 0;
+        this.unit = unit;
+    }
+
+    public String getUnit(){
+        return  unit;
+    }
+
+    public void setCount(int count){
+        this.count = count;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
