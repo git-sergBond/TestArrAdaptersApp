8ac6af944781ac4816beef75ad028f0114eb04dc
git-sergBond <git-serg.bond@yandex.ru>
2017-06-28 12:21:12 +0400

    sucessful menu

--- app/src/main/java/com/example/home/testarradaptersapp/MainActivity.java
+++ app/src/main/java/com/example/home/testarradaptersapp/MainActivity.java
@@ -2,28 +2,64 @@ package com.example.home.testarradaptersapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Product> products = new ArrayList();
-
+    String[] cities = {"Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
     ListView productList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(products.size() == 0){
+        /*if(products.size() == 0){
             products.add(new Product("Картофель", "кг."));
             products.add(new Product("Чай", "шт."));
             products.add(new Product("Яйца", "шт."));
             products.add(new Product("Молоко", "л."));
             products.add(new Product("Макароны", "кг."));
         }
+
         setContentView(R.layout.activity_main);
         productList = (ListView)findViewById(R.id.productList);
         ProductAdapter adapter = new ProductAdapter(this,R.layout.list_item,products);
         productList.setAdapter(adapter);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.cities);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cities);
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Применяем адаптер к элементу spinner
+        spinner.setAdapter(adapter);
+
+
+        AutoCompleteTextView textComplete = (AutoCompleteTextView)findViewById(R.id.automplite);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,cities);
+        textComplete.setAdapter(adapter);
+  */
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu1,menu);
+        menu.add(2,0,0,"asdeqe");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(!item.isChecked()) item.setChecked(true); else item.setChecked(false);
+        Toast.makeText(this,Integer.toString(id),Toast.LENGTH_SHORT).show();
+        return true;
+        //return super.onOptionsItemSelected(item);
     }
 }
